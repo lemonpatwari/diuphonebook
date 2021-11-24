f590c88d2d9014b5370651e90c7be68753ba1e20
Lemon Patwari <lemonpatwari@gmail.com>
2021-11-24 15:57:44 +0600

    route riderect

--- app/Http/Controllers/PhonebookController.php
+++ app/Http/Controllers/PhonebookController.php
@@ -24,7 +24,7 @@ class PhonebookController extends Controller
         }
 
         $phonebooks=$phonebooks->get();
-        
+
         return view('phonebook.index', compact('phonebooks'));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::redirect('/', '/phonebook');
 
 Route::resource('phonebook', PhonebookController::class);
 
